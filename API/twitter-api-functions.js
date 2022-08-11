91859e9084e918784007b8869a8de7652bfc2cef
Julianmenav <julianmenav2@gmail.com>
2022-08-11 16:05:10 +0200

    Transferring logic to an http server using Express

--- API/twitter-api-functions.js
+++ API/twitter-api-functions.js
@@ -2,15 +2,17 @@ const axios = require('axios')
 
 
 const getTweet = async (strId) => {
-  const tweetResponse = await axios
-    .get(`https://api.twitter.com/1.1/statuses/lookup.json?id=${strId}`,
-      { headers: { "Authorization": process.env.BEARER } })
-    .catch(e => {
-      console.error(e.response.data)
-    })
-  const variantsObj = tweetResponse.data[0].extended_entities.media[0].video_info.variants
+  try {
+    const tweetResponse = await axios
+      .get(`https://api.twitter.com/1.1/statuses/lookup.json?id=${strId}`,
+        { headers: { "Authorization": process.env.BEARER } })
+    const variantsObj = tweetResponse.data[0].extended_entities.media[0].video_info.variants
 
-  return variantsObj
+    return variantsObj
+
+  } catch (error) {
+    console.error(error)
+  }
 }
 
 
--- server.js
+++ server.js
@@ -1,17 +1,40 @@
 require('dotenv').config()
-const {getTweet} = require("./API/twitter-api-functions")
-const {getBestVideo} = require('./utils/functions')
+const express = require('express')
+const { getTweet } = require("./API/twitter-api-functions")
+const { getBestVideo } = require('./utils/functions')
 
-const url = process.argv[2]
-const splittedURL = url.split("/")
-const tweetId = splittedURL[splittedURL.length - 1]
-console.log("El id del tweet es "+ tweetId)
-console.log()
-console.log("Link al video:")
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.use(express.urlencoded({ extended: false }))
 
 
-getTweet(tweetId).then(data => {
-  const best = getBestVideo(data)
-  console.log(best.url)
+app.get("/", (req, res) => {
+  res.send("HTML")
 })
 
+
+app.get("/api/:tweetId", (req, res) => {
+  const { tweetId } = req.params
+
+  getTweet(tweetId).then(data => {
+
+    if (data) {
+      const best = getBestVideo(data)
+      res.json({ "Link al vídeo": best.url })
+    } else {
+      //Cuando hay un error procesando el tweet.
+      res.json({ "Error": "Link inválido" })
+    }
+  })
+})
+
+//Not Found Middleware
+
+app.use((req, res) => res.status(404).json({ "Error": "Esta ruta no existe." }))
+
+
+app.listen(port, function () {
+  console.log(`Listening on port ${port}`);
+});
