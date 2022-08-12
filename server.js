8d8e0ab5de1217340e999eb03c89d92cb5671c5e
Julianmenav <julianmenav2@gmail.com>
2022-08-12 18:25:52 +0200

    refactor logic to a middleware function

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const { getTweet } = require("./API/twitter-api-functions")
-const { getBestVideo } = require('./utils/functions')
+const { getLink } = require('./middleware/middleware')
 
 const app = express();
 
@@ -15,43 +14,7 @@ app.get('/', function (req, res) {
 });
 
 
-app.post("/api/url", (req, res) => {
-  const { url } = req.body
-
-  //Get tweet ID
-  const splittedUrl = url.split("/")
-  let tweetId
-
-  if (splittedUrl[splittedUrl.length - 1] == "") {
-    tweetId = splittedUrl[splittedUrl.length - 2]
-  } else {
-    tweetId = splittedUrl[splittedUrl.length - 1]
-  }
-
-
-
-  //API CALL
-  getTweet(tweetId).then(tweetResponse => {
-
-    //response = undefined
-    if (!tweetResponse) {
-      return res.status(400).json({ "Error": `La URL '${url}' no es de un tweet válido, o no contiene un vídeo` })
-    }
-    //Response = Error
-    if (tweetResponse == "ERR_BAD_REQUEST") {
-      return res.status(400).json({ "Error": "Twitter API Error, por favor avise a Julian" })
-    }
-
-    try {
-      const variantsObj = tweetResponse.extended_entities.media[0].video_info.variants
-      const bestVideo = getBestVideo(variantsObj)
-      res.json({ "Link al vídeo": bestVideo.url })
-    } catch (error) {
-      console.error(error)
-      res.status(400).json({ "Error": error })
-    }
-  })
-})
+app.post("/api/url", getLink)
 
 //Not Found Middleware
 
